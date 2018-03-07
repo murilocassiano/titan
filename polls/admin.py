ed7d8de90ad8e4c343bdbecbd977dd62c9682eba
murilocassiano <murilo.cassiano@luizalabs.com>
2018-03-07 20:37:02 -0300

    database dump

--- polls/admin.py
+++ polls/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 from .models import Asset
-
 from .models import Transaction
+from .models import Historic
 
 admin.site.register(Asset)
 
 admin.site.register(Transaction)
+
+admin.site.register(Historic)
--- polls/models.py
+++ polls/models.py
@@ -3,83 +3,47 @@ import datetime
 
 class Asset(models.Model):
     asset_name = models.CharField(
-    	max_length=200,
-    	unique=True,
-    	verbose_name="Nome do Ativo"
-    	)
+        max_length=200,
+        verbose_name="Nome do Ativo"
+    )
 
     asset_code = models.CharField(
-    	max_length=200,
-    	unique=True,
-    	verbose_name="Código do Ativo"
-    	)
-
-class Date(models.Model):
-    date =  models.DateTimeField(
-        default= datetime.date(1000,1,1),
-        unique=True)
-
-    weekday = models.CharField(
-        max_length=20,
-        choices = [
-           ('0', 'Monday'),
-           ('1', 'Tuesday'),
-           ('2', 'Wednesday'),
-           ('3', 'Thursday'),
-           ('4', 'Friday'),
-           ('5', 'Saturday'),
-           ('6', 'Sunday')
-        ],
-    ) 
-
-    month = models.CharField(
-        max_length=20,
-        choices = [
-           ('1', 'Jan'),
-           ('2', 'Feb'),
-           ('3', 'Mar'),
-           ('4', 'Apr'),
-           ('5', 'May'),
-           ('6', 'Jun'),
-           ('7', 'Jul'),
-           ('8', 'Ago'),
-           ('9', 'Sep'),
-           ('10', 'Oct'),
-           ('11', 'Nov'),
-           ('12', 'Dec'),
-        ],
-    ) 
+        max_length=200,
+        unique=True,
+        verbose_name="Código do Ativo"
+    )
 
 class Transaction(models.Model):
+    asset = models.ForeignKey(Asset)
+
     nature = models.CharField(
         max_length=20,
-        choices = [('CP', 'Compra'), ('VD', 'Venda')],
+        choices = (('CP', 'Compra'), ('VD', 'Venda')),
         default = "CP"
     )
-    price = models.FloatField(
-        verbose_name="Valor Transação"
+
+    unit_price = models.FloatField(
+        verbose_name="Valor unitário da Transação"
     )
 
     quantity = models.FloatField(
         verbose_name="Quantidade"
     )
-    class Meta:
-        unique_together = ('nature','price', 'quantity')
 
-class AssetsAnalytics(models.Model):
-    assetId = models.ForeignKey(Asset)
-
-    dateId = models.ForeignKey(Date)
-
-    transactionId = models.ForeignKey(
-        Transaction,
-        default = 0)
-
-    tpe = models.CharField(
-        max_length=20,
-        choices =[('TR', 'Transactional'), ('DV', 'Daily Value')],
-        default = "DV"
-        )
+    transaction_date = models.DateTimeField(
+        default=datetime.date(1000,1,1)
+    )
 
     class Meta:
-        unique_together = ('assetId','dateId', 'transactionId', 'tpe')
+        unique_together = ('asset','unit_price','nature','quantity', 'transaction_date')
+
+
+class Historic(models.Model):
+    asset = models.ForeignKey(Asset)
+    day_price = models.FloatField(
+        verbose_name="preço do dia"
+    )
+
+    date = models.DateTimeField(
+        default=datetime.date(1000,1,1)
+    )    
